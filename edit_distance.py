7260c98dc421b0d9542ba737c9a935d72c4ee17b
buzaev <fbuzaev@gmail.com>
2021-07-16 00:21:43 +0300

    Add edit_distance.py and inversions.py

--- edit_distance.py
+++ edit_distance.py
@@ -49,9 +49,24 @@ distance
 
 
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    d = [[x] + [0] * (len(t)) for x in range(len(s) + 1)]
+    d[0] = [x for x in range(len(t)+1)]
+
+    for j in range(1, len(t) + 1):
+        for i in range(1, len(s) + 1):
+            insertion = d[i][j-1] + 1
+            deletion = d[i-1][j] + 1
+            match = d[i-1][j-1]
+            mismatch = d[i-1][j-1] + 1
+            if s[i-1] == t[j-1]:
+                d[i][j] = min(insertion, deletion, match)
+            else:
+                d[i][j] = min(insertion, deletion, mismatch)
+    return d[-1][-1]
 
 
 if __name__ == "__main__":
+    # s = input()
+    # t = input()
+    # print(edit_distance(s, t))
     print(edit_distance(input(), input()))
